fbbdb9e3823d82aee37d0ddb61dbbc940320f1c6
Nicholas Hwang <hello@undefined.engineer>
2017-12-15 14:58:20 -0500

    :shit:

--- symlink.sh
+++ symlink.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
-DOTFILES_DIR="$HOME/.dotfiles"
-TMP_DIR="$DOTFILES_DIR/.tmp"
-MACOS_DIR="Library/Application Support"
+DOTFILES_DIR=$HOME/.dotfiles
+TMP_DIR=$DOTFILES_DIR/.tmp
 
 declare -A OUTPUTS=(
   # dotfiles
   ["gitconfig"]=".gitconfig"
   ["gitignore"]=".gitignore"
-  ["hammerspoon"]=".hammerspoon"
   ["hushlogin"]=".hushlogin"
   ["tmux.conf"]=".tmux.conf"
   ["tmuxline.conf"]=".tmuxline.conf"
   ["vim"]=".vim"
   ["vimrc"]=".vimrc"
+  ["xinitrc"]=".xinitrc"
   ["zshrc"]=".zshrc"
 
-  # settings
-  ["settings.json"]="$MACOS_DIR/Code/User/settings.json"
+  # configs
+  ["i3"]=".config/i3"
+  ["i3blocks"]=".config/i3blocks"
+  ["rofi"]=".config/rofi"
+  ["yay"]=".config/yay"
+
+  # simple terminal
+  ["st/config.h"]=".pkg/st/config.h"
 )
 
 echo "Clearing previously cached..."
