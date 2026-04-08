9774e88ce23be96ceedf9b7063e928b4a8bb0ce1
Nicholas Hwang <git@undefined.engineer>
2026-04-08 00:03:10 -0700

    🏎️

--- plugins/nvm.zsh
+++ plugins/nvm.zsh
@@ -1,5 +1,15 @@
 export NVM_DIR="$HOME/.nvm"
 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# Lazy-load nvm: only source nvm.sh when nvm/node/npm/npx is first called
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  _nvm_lazy_load() {
+    unset -f nvm node npm npx
+    \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+  }
 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+  nvm()  { _nvm_lazy_load; nvm  "$@"; }
+  node() { _nvm_lazy_load; node "$@"; }
+  npm()  { _nvm_lazy_load; npm  "$@"; }
+  npx()  { _nvm_lazy_load; npx  "$@"; }
+fi
