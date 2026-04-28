2f0a580fc5f02a29aba44cc217bc7d2aa4d23123
Nicholas Hwang <git@undefined.engineer>
2026-04-27 17:39:30 -0700

    🌑

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,19 +4,37 @@ return {
   cmd = { "ConformInfo", "Prettier" },
   config = function()
     local conform = require("conform")
+    local util = require("conform.util")
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { "prettier", stop_after_first = true },
-        typescript = { "prettier", stop_after_first = true },
-        typescriptreact = { "prettier", stop_after_first = true },
+        javascript = { "eslint", "prettier" },
+        javascriptreact = { "eslint", "prettier" },
+        typescript = { "eslint", "prettier" },
+        typescriptreact = { "eslint", "prettier" },
         json = { "prettier", stop_after_first = true },
         lua = { "stylua" },
         css = { "prettier" },
         html = { "prettier" },
       },
-      format_on_save = {
-        timeout_ms = 2000,
+      formatters = {
+        eslint = {
+          command = util.from_node_modules("eslint"),
+          args = { "--fix", "--no-warn-ignored", "$FILENAME" },
+          stdin = false,
+          tmpfile_format = "$FILENAME",
+          cwd = util.root_file({
+            "eslint.config.js",
+            "eslint.config.cjs",
+            ".eslintrc",
+            ".eslintrc.js",
+            ".eslintrc.cjs",
+            "package.json",
+          }),
+          require_cwd = true,
+        },
+      },
+      format_after_save = {
         lsp_format = "fallback",
       },
     })
