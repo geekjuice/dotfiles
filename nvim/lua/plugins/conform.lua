4f2d855e67c7891269f71746b684eb35ed8c99d1
Nicholas Hwang <git@undefined.engineer>
2026-04-28 22:03:39 -0700

    🚯

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -8,10 +8,14 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { "eslint", "prettier" },
-        javascriptreact = { "eslint", "prettier" },
-        typescript = { "eslint", "prettier" },
-        typescriptreact = { "eslint", "prettier" },
+        -- javascript = { "eslint", "prettier" },
+        -- javascriptreact = { "eslint", "prettier" },
+        -- typescript = { "eslint", "prettier" },
+        -- typescriptreact = { "eslint", "prettier" },
+        javascript = { "prettier", stop_after_first = true },
+        javascriptreact = { "prettier", stop_after_first = true },
+        typescript = { "prettier", stop_after_first = true },
+        typescriptreact = { "prettier", stop_after_first = true },
         json = { "prettier", stop_after_first = true },
         lua = { "stylua" },
         css = { "prettier" },
@@ -34,9 +38,9 @@ return {
           require_cwd = true,
         },
       },
-      format_after_save = {
-        lsp_format = "fallback",
-      },
+      -- format_after_save = {
+      --   lsp_format = "fallback",
+      -- },
     })
 
     vim.api.nvim_create_user_command("Prettier", function()
